07a6e54b6f1d5cee8b0c37638d9b261c8de6abe4
erikatests <kurikurryko@gmail.com>
2023-06-05 23:41:01 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 'use strict';
+function fibseq(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }
+    return fibseq(n-1) + fibseq(n-2);
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fibseq(i));
+}
